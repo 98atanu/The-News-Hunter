b851172d59bd63c29aef8b7eeb819ab15ef57670
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2023-12-26 20:56:33 +0530

    Updated the headline.

--- src/App.js
+++ src/App.js
@@ -10,20 +10,21 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 6;
   render() {
     return (
       <div>
         <Router>
         <NavBar/>
           <Routes>    
-            <Route exact path = "/" element = {<News key= "General" pageSize={5} country= {"in"} category = {'General'}/>}/>
-            <Route exact path = "/business"  element = {<News key= "Business" pageSize={5} country= {"in"} category = {'Business'}/>}/>
-            <Route exact path = "/entertainment"  element = {<News key= "Entertainment" pageSize={5} country= {"in"} category = {'Entertainment'}/>}/>
-            <Route exact path = "/general"  element = {<News key= "General" pageSize={5} country= {"in"} category = {'General'}/>}/>
-            <Route exact path = "/health" key= "Health" element = {<News pageSize={5} country= {"in"} category = {'Health'}/>}/>
-            <Route exact path = "/science"  element = {<News key= "Science" pageSize={5} country= {"in"} category = {'Science'}/>}/>
-            <Route exact path = "/sports"  element = {<News key= "Sports" pageSize={5} country= {"in"} category = {'Sports'}/>}/>
-            <Route exact path = "/technology"  element = {<News key= "Technology" pageSize={5} country= {"in"} category = {'Technology'}/>}/>
+            <Route exact path = "/" element = {<News key= "General" pageSize={this.pageSize} country= {"in"} category = {'General'}/>}/>
+            <Route exact path = "/business"  element = {<News key= "Business" pageSize={this.pageSize} country= {"in"} category = {'Business'}/>}/>
+            <Route exact path = "/entertainment"  element = {<News key= "Entertainment" pageSize={this.pageSize} country= {"in"} category = {'Entertainment'}/>}/>
+            <Route exact path = "/general"  element = {<News key= "General" pageSize={this.pageSize} country= {"in"} category = {'General'}/>}/>
+            <Route exact path = "/health" key= "Health" element = {<News pageSize={this.pageSize} country= {"in"} category = {'Health'}/>}/>
+            <Route exact path = "/science"  element = {<News key= "Science" pageSize={this.pageSize} country= {"in"} category = {'Science'}/>}/>
+            <Route exact path = "/sports"  element = {<News key= "Sports" pageSize={this.pageSize} country= {"in"} category = {'Sports'}/>}/>
+            <Route exact path = "/technology"  element = {<News key= "Technology" pageSize={this.pageSize} country= {"in"} category = {'Technology'}/>}/>
           </Routes>
         </Router>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,23 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-
-    
   render() {
-    let {title,description,imageUrl,newsUrl,author,date} = this.props
+    let { title, description, imageUrl, newsUrl, author, date } = this.props;
     return (
-      <div className='my-3'>
-        <div className="card" >
-  <img src={!imageUrl?"https://cms.therecord.media/uploads/pexels_photomix_company_5494323_4f252d0a34.jpg":imageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <p className="card-text"><small>By {!author?"Unknown":author} on {new Date(date).toTimeString()}</small></p>
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://cms.therecord.media/uploads/pexels_photomix_company_5494323_4f252d0a34.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small>
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toTimeString()}
+              </small>
+            </p>
 
-    <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
-  </div>
-</div>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
